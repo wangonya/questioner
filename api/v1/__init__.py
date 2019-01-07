165dd16db1c7eca65a89be8b87181266efbe7c82
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-07 22:52:22 +0300

    add v1 folder as package to hold code for first version of api

---
+++ api/v1/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.cfg')
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
