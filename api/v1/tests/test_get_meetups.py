d7b9f9ca81ba9b1976ca78cc0598ec1405325714
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-08 18:04:57 +0300

    [starts #163026393] write tests for getting specific meetup

---
+++ api/v1/tests/test_get_meetups.py
@@ -0,0 +1,11 @@
+from v1.tests import main
+
+
+def test_get_upcoming_meetups(main):
+    res = main.get('/api/v1/meetups/upcoming')
+    assert res.status_code == 200
+
+
+def test_specific_meetup(main):
+    res = main.get('/api/v1/meetups/1')
+    assert res.status_code == 200
