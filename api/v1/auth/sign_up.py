65b5aea9c58640569d6d9d88fedbd17ff6f2bad7
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-08 10:29:32 +0300

    [finishes #163047147] improve structure of returns and exception handling

--- api/v1/auth/sign_up.py
+++ api/v1/auth/sign_up.py
@@ -8,25 +8,45 @@ sign_up = Blueprint('sign_up', __name__, url_prefix='/api/v1/auth')
 
 @sign_up.route('/signup', methods=['POST'])
 def user_signup():
-    # make sure there's data and its properly formatted
-    if None or not request.json:
+
+    try:
+        email = request.json['email']
+        password = request.json['password']
+
+        # check if email already exists to avoid duplicates
+        if any(u['email'] == email for u in models.users):
+            return jsonify(
+                {
+                    "status": 409,
+                    "error": "email already exists"
+                }), 409
+
+        # check that data is in a correct format
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            return jsonify(
+                {
+                    "status": 400,
+                    "error": "email format invalid"
+                }), 400
+        elif 6 > len(password) or 12 < len(password):
+            return jsonify(
+                {
+                    "status": 422,
+                    "error": "password length invalid"
+                }), 422
+        else:
+            user = {
+                'email': email,
+                'password': generate_password_hash(password)
+            }
+            models.users.append(user)
+            return jsonify(
+                {
+                    "status": 201,
+                    "data": [{
+                        "msg": "user registered successfully",
+                        "email": email
+                    }]
+                }), 201
+    except (ValueError, KeyError, TypeError):
         abort(400)
-
-    email = request.json['email']
-    password = request.json['password']
-
-    # check if email already exists to avoid duplicates
-    if any(u['email'] == email for u in models.users):
-        return jsonify({'msg': 'email already exists'}), 409
-
-    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-        return jsonify({'msg': 'email format invalid'}), 400
-    elif 6 > len(password) or 12 < len(password):
-        return jsonify({'msg': 'password length invalid'}), 422
-    else:
-        user = {
-            'email': email,
-            'password': generate_password_hash(password)
-        }
-        models.users.append(user)
-        return jsonify({'user': user}), 201
