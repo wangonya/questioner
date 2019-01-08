63f33eccba3c7cddec9c0150b57856d21bc09b56
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-08 12:04:01 +0300

    add code for login endpoint

---
+++ api/v1/auth/log_in.py
@@ -0,0 +1,47 @@
+from flask import request, abort, jsonify, Blueprint
+from werkzeug.security import check_password_hash
+from v1.auth import models
+import re
+
+log_in = Blueprint('log_in', __name__, url_prefix='/api/v1/auth')
+
+
+@log_in.route('/login', methods=['POST'])
+def user_login():
+
+    try:
+        email = request.json['email']
+        password = request.json['password']
+
+        # check that data is in a correct format
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            return jsonify(
+                {
+                    "status": 400,
+                    "error": "email format invalid"
+                }), 400
+        if any(u['email'] == email for u in models.users):
+            get_user_password = [u['password'] for u in models.users if u['email'] == email]
+            if check_password_hash(get_user_password[0], password):
+                return jsonify(
+                    {
+                        "status": 200,
+                        "data": [{
+                            "msg": "user logged in successfully",
+                            "email": email
+                        }]
+                    }), 200
+            else:
+                return jsonify(
+                    {
+                        "status": 401,
+                        "error": "wrong password"
+                    }), 401
+        else:
+            return jsonify(
+                {
+                    "status": 401,
+                    "error": "that account does not exist"
+                }), 401
+    except (ValueError, KeyError, TypeError):
+        abort(400)
