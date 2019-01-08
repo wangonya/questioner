cd0fceb8ef999762b6bae8cc55e2e832f2464a7a
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-08 12:23:18 +0300

    [starts #163025981] write tests for reset password endpoint

--- api/v1/tests/test_auth.py
+++ api/v1/tests/test_auth.py
@@ -53,3 +53,27 @@ def test_log_in_invalid_password(main):
     }
     res = post_json(main, '/api/v1/auth/login', test_data)
     assert res.status_code == 401
+
+
+def test_reset_valid(main):
+    test_data = {
+        'email': 'kwangonya@gmail.com'
+    }
+    res = post_json(main, '/api/v1/auth/reset', test_data)
+    assert res.status_code == 200
+
+
+def test_reset_invalid_email(main):
+    test_data = {
+        'email': 'kwangonyagmail.com'
+    }
+    res = post_json(main, '/api/v1/auth/reset', test_data)
+    assert res.status_code == 400
+
+
+def test_reset_invalid(main):
+    test_data = {
+        'email': 'kwango@gmail.com'
+    }
+    res = post_json(main, '/api/v1/auth/reset', test_data)
+    assert res.status_code == 401
