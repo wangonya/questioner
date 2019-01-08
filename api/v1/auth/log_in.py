424f241c4e279792f6b02cd0e9ffa264ceb6a885
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-08 19:33:51 +0300

    return access token on successful login

--- api/v1/auth/log_in.py
+++ api/v1/auth/log_in.py
@@ -1,5 +1,6 @@
 from flask import request, abort, jsonify, Blueprint
 from werkzeug.security import check_password_hash
+from flask_jwt_extended import create_access_token
 from v1.auth import models
 import re
 
@@ -23,12 +24,14 @@ def user_login():
         if any(u['email'] == email for u in models.users):
             get_user_password = [u['password'] for u in models.users if u['email'] == email]
             if check_password_hash(get_user_password[0], password):
+                access_token = create_access_token(identity=email)
                 return jsonify(
                     {
                         "status": 200,
                         "data": [{
                             "msg": "user logged in successfully",
-                            "email": email
+                            "email": email,
+                            "access_token": access_token
                         }]
                     }), 200
             else:
