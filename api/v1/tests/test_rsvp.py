6330074079343f1291b9c5e9b1be6c4c8217fef9
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-09 22:30:57 +0300

    [starts #163028224] add tests for rsvp endpoint

---
+++ api/v1/tests/test_rsvp.py
@@ -0,0 +1,9 @@
+from v1.tests import main, post_json
+
+
+def test_rsvp_meetup(main):
+    test_data = {
+            "status": "maybe"
+        }
+    res = post_json(main, '/api/v1/meetups/1/rsvps', test_data)
+    assert res.status_code == 201 or res.status_code == 200
