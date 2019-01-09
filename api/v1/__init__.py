dea2a6b30b11c95c81cc87ed3c49f67582774ee6
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-09 18:34:29 +0300

    add fallback config to fix config not found error in travis

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from v1.auth.sign_up import sign_up
@@ -9,10 +10,26 @@ from v1.questions.post_questions import post_q
 from v1.questions.vote import upvote_q
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.cfg')
     jwt = JWTManager(app)
+
+    app.config.from_mapping(
+        SECRET_KEY='questioner-dev-secret-key',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.cfg', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route('/')
     def hello():
