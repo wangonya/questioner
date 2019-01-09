c3371fc9ff3a74c7177c97629d251c0f39876af4
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-09 12:36:26 +0300

    [starts #163027999] add tests for post meetup

---
+++ api/v1/tests/test_post_meetup.py
@@ -0,0 +1,13 @@
+from v1.tests import main, post_json
+
+
+def test_post_meetup(main):
+    test_data = {
+            "title": "sample meetup",
+            "location": "test location",
+            "happeningOn": "test date",
+            "tags": ["tag1", "tag2", "tag3"],
+            "image": ""
+        }
+    res = post_json(main, '/api/v1/meetups', test_data)
+    assert res.status_code == 201
