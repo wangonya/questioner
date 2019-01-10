11914afc349bc84d23384e2077b988379277bbf5
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-10 09:30:13 +0300

    update tests for voting to add downvote test and handle assertions better

--- api/v1/tests/test_vote_question.py
+++ api/v1/tests/test_vote_question.py
@@ -1,7 +1,13 @@
 from v1.tests import main, patch_json
 
 
-def test_post_question(main):
+def test_upvote_question(main):
     test_data = {}
-    res = patch_json(main, '/api/v1/questions/<int:q_id>/upvote', test_data)
-    assert res.status_code == 201 or 200
+    res = patch_json(main, '/api/v1/questions/1/upvote', test_data)
+    assert res.status_code == 201 or res.status_code == 200
+
+
+def test_downvote_question(main):
+    test_data = {}
+    res = patch_json(main, '/api/v1/questions/1/downvote', test_data)
+    assert res.status_code == 201 or res.status_code == 200
