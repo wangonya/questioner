48a5dbb19f98218947d570d15dec8238e25b4a3a
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-10 09:31:14 +0300

    [finishes #163027064] register blueprint

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -8,7 +8,7 @@ from v1.meetups.view_meetups import upcoming_meetups, specific_meetup
 from v1.meetups.create_meetup import create_m
 from v1.meetups.rsvp import rsvp_m
 from v1.questions.post_questions import post_q
-from v1.questions.vote import upvote_q
+from v1.questions.vote import upvote_q, downvote_q
 
 
 def create_app(test_config=None):
@@ -46,5 +46,6 @@ def create_app(test_config=None):
     app.register_blueprint(upvote_q)
     app.register_blueprint(create_m)
     app.register_blueprint(rsvp_m)
+    app.register_blueprint(downvote_q)
 
     return app
