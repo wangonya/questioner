50e046c536ffd1928c975df2b3f561f91d389a82
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-10 12:34:53 +0300

    [starts #163027613] add tests for answer question endpoint

---
+++ api/v1/tests/test_question_answer.py
@@ -0,0 +1,14 @@
+from v1.tests import main, post_json
+
+
+def test_answer_question(main):
+    test_data = {
+        "id": 1,
+        "createdOn": "some date",
+        "createdBy": 1,
+        "meetup": 1,
+        "body": "sample answer body",
+        "forQuestion": 1
+    }
+    res = post_json(main, '/api/v1/questions/1/answer', test_data)
+    assert res.status_code == 201
