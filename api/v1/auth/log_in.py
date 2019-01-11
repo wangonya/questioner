e64f29de62125cb717099c360f042c23601d544f
Kinyanjui Wangonya <kwangonya@gmail.com>
2019-01-11 07:50:00 +0300

    fix importation error due to folder structure change

--- api/v1/auth/log_in.py
+++ api/v1/auth/log_in.py
@@ -1,7 +1,7 @@
 from flask import request, abort, jsonify, Blueprint
 from werkzeug.security import check_password_hash
 from flask_jwt_extended import create_access_token
-from v1.auth import models
+from api.v1.auth import models
 import re
 
 log_in = Blueprint('log_in', __name__, url_prefix='/api/v1/auth')
