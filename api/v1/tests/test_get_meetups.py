ea5b08f9cc00a48aa94cfee7a3ba0fbd0670bce9
wangonya <kwangonya@gmail.com>
2019-01-11 08:35:07 +0300

    [starts #163027929] add tests for get meetups

--- api/v1/tests/test_get_meetups.py
+++ api/v1/tests/test_get_meetups.py
@@ -1,11 +1,23 @@
 from api.v1.tests import main
 
 
-def test_get_upcoming_meetups(main):
+def test_get_no_upcoming_meetups(main):
     res = main.get('/api/v1/meetups/upcoming')
     assert res.status_code == 200
+
+
+def test_no_specific_meetup(main):
+    res = main.get('/api/v1/meetups/54')
+    assert res.status_code == 404
 
 
 def test_specific_meetup(main):
     res = main.get('/api/v1/meetups/1')
     assert res.status_code == 200
+
+
+def test_admin_meetup(main):
+    res = main.get('/api/v1/admin/profile/1')
+    assert res.status_code == 200 or res.status_code == 401
+
+
