71b1df20b967b2f2d6958b2619ca006be06183cd
wangonya <kwangonya@gmail.com>
2019-01-12 17:44:36 +0300

    edit code to implement oop

--- api/v1/auth/models.py
+++ api/v1/auth/models.py
@@ -1,1 +1,26 @@
-users = []
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+
+
+class AuthModel:
+    user_model = []
+
+    def __init__(self, firstname, lastname, othername, username,
+                 email, phonenumber, password, isadmin):
+        self.uid = len(AuthModel.user_model) + 1
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.phonenumber = phonenumber
+        self.othername = othername
+        self.username = username
+        self.password = generate_password_hash(password)
+        self.isadmin = isadmin
+        self.registered = datetime.now()
+
+    def save_to_db(self):
+        AuthModel.user_model.append(self)
+
+    @classmethod
+    def find_by_email(cls, email):
+        return any(u.email == email for u in AuthModel.user_model)
