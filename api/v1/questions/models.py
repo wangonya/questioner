0f8fc58cea3a0382bda4d79eb6034629d423cee1
wangonya <kwangonya@gmail.com>
2019-01-13 18:25:25 +0300

    extend model by adding answers class

--- api/v1/questions/models.py
+++ api/v1/questions/models.py
@@ -1,18 +1,20 @@
 from datetime import datetime
 
+from ...error_handlers import DataIndexError
+
 
 class PostQuestionsModel:
     questions = [
         {
           "body": "test body.",
-          "id": 1,
+          "q_id": 1,
           "meetup": 1,
           "title": "test"
         }
     ]
     
     def __init__(self, title, creator, body, meetup):
-        self.m_id = len(PostQuestionsModel.questions) + 1
+        self.q_id = len(PostQuestionsModel.questions) + 1
         self.title = title
         self.creator = creator
         self.body = body
@@ -23,9 +25,35 @@ class PostQuestionsModel:
     def save_question_to_db(self):
         PostQuestionsModel.questions.append(self)
 
+    @classmethod
+    def find_by_q_id(cls, q_id):
+        try:
+            question = [question for question in cls.questions if question["q_id"] == q_id][0]
+        except IndexError:
+            raise DataIndexError
+        return question
+
+    @classmethod
+    def find_meetup_by_q_id(cls, q_id):
+        try:
+            meetup = [q["meetup"] for q in cls.questions if q["q_id"] == q_id][0]
+        except IndexError:
+            raise DataIndexError
+        return meetup
+
 
 class AnswerQuestionsModel:
-    pass
+    answers = []
+
+    def __init__(self, body, creator, meetup, for_question):
+        self.a_id = len(AnswerQuestionsModel.answers) + 1
+        self.body = body
+        self.creator = creator
+        self.meetup = meetup
+        self.for_question: for_question
+
+    def save_answer_to_db(self):
+        AnswerQuestionsModel.answers.append(self)
 
 
 class VoteModel:
