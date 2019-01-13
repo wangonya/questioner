acb5e2d028847ab7fe3e27e27765fd4dd7eecc49
wangonya <kwangonya@gmail.com>
2019-01-13 11:11:35 +0300

    add user recognition helper methods and add register user as default admin for testing purposes

--- api/v1/auth/models.py
+++ api/v1/auth/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from ...error_handlers import DataIndexError
+from ...error_handlers import DataIndexError, AdminProtectedError
 
 
 class AuthModel:
@@ -15,9 +15,9 @@ class AuthModel:
         self.email = email
         self.phonenumber = phonenumber
         self.othername = othername
-        self.username = email.split('@')[0],
+        self.username = email.split("@")[0],
         self.password = generate_password_hash(password)
-        self.isadmin = False
+        self.isadmin = True
         self.registered = datetime.now()
 
     def save_to_db(self):
@@ -26,6 +26,22 @@ class AuthModel:
     @classmethod
     def find_by_email(cls, email):
         return any(u.email == email for u in cls.user_model)
+
+    @classmethod
+    def find_by_uid(cls, user):
+        try:
+            userid = [u.uid for u in cls.user_model if u.email == user][0]
+        except IndexError:
+            raise DataIndexError
+        return userid
+
+    @classmethod
+    def check_if_admin(cls, user):
+        try:
+            is_admin = [u.isadmin for u in cls.user_model if u.email == user][0]
+        except IndexError:
+            raise AdminProtectedError
+        return is_admin
 
     @classmethod
     def verify_hash(cls, email, unhashed):
