5039aa7483ef3220182c6aeee2194565dfe3fcd2
wangonya <kwangonya@gmail.com>
2019-01-13 16:10:28 +0300

    edit to imlpement oop

--- api/v1/questions/models.py
+++ api/v1/questions/models.py
@@ -1,10 +1,32 @@
-questions = [
-    {
-      "body": "test body.",
-      "id": 1,
-      "meetup": 1,
-      "title": "test"
-    }
-]
-votes = []
-answers = []
+from datetime import datetime
+
+
+class PostQuestionsModel:
+    questions = [
+        {
+          "body": "test body.",
+          "id": 1,
+          "meetup": 1,
+          "title": "test"
+        }
+    ]
+    
+    def __init__(self, title, creator, body, meetup):
+        self.m_id = len(PostQuestionsModel.questions) + 1
+        self.title = title
+        self.creator = creator
+        self.body = body
+        self.meetup = meetup
+        self.created_on = datetime.now()
+        self.votes = 0
+
+    def save_question_to_db(self):
+        PostQuestionsModel.questions.append(self)
+
+
+class AnswerQuestionsModel:
+    pass
+
+
+class VoteModel:
+    pass
