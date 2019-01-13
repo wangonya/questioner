612df1001f223c883c1769ad4a5646f01eee4d22
wangonya <kwangonya@gmail.com>
2019-01-13 20:07:23 +0300

    add vote class model and add helper class methods

--- api/v1/questions/models.py
+++ api/v1/questions/models.py
@@ -41,6 +41,22 @@ class PostQuestionsModel:
             raise DataIndexError
         return meetup
 
+    @classmethod
+    def find_title_by_q_id(cls, q_id):
+        try:
+            title = [q["title"] for q in cls.questions if q["q_id"] == q_id][0]
+        except IndexError:
+            raise DataIndexError
+        return title
+
+    @classmethod
+    def find_body_by_q_id(cls, q_id):
+        try:
+            body = [q["body"] for q in cls.questions if q["q_id"] == q_id][0]
+        except IndexError:
+            raise DataIndexError
+        return body
+
 
 class AnswerQuestionsModel:
     answers = []
@@ -57,4 +73,59 @@ class AnswerQuestionsModel:
 
 
 class VoteModel:
-    pass
+    votes = []
+
+    @classmethod
+    def save_upvote_to_db(cls, voter, question):
+        upvote = {
+            "user": voter,
+            "question": question,
+            "count": 1
+        }
+
+        downvote = {
+            "user": voter,
+            "question": question,
+            "count": -1
+        }
+
+        # check if user has already voted
+        # if they have, remove the record
+        # if not, append
+        if any(v["user"] == voter and v["question"] == question for v in cls.votes):
+            try:
+                cls.votes.remove(upvote)
+            except ValueError:  # the vote was a downvote, so remove that instead
+                cls.votes.remove(downvote)
+        else:
+            cls.votes.append(upvote)
+
+        return sum(s["count"] for s in cls.votes)
+
+    @classmethod
+    def save_downvote_to_db(cls, voter, question):
+        upvote = {
+            "user": voter,
+            "question": question,
+            "count": 1
+        }
+
+        downvote = {
+            "user": voter,
+            "question": question,
+            "count": -1
+        }
+
+        # check if user has already voted
+        # if they have, remove the record
+        # if not, append
+        if any(v["user"] == voter and v["question"] == question for v in cls.votes):
+            try:
+                cls.votes.remove(downvote)
+            except ValueError:  # the vote was a upvote, so remove that instead
+                cls.votes.remove(upvote)
+        else:
+            cls.votes.append(downvote)
+
+        return sum(s["count"] for s in cls.votes)
+
