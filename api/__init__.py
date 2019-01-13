06459b812d964967c0c562a97582958f9dfc5399
wangonya <kwangonya@gmail.com>
2019-01-13 20:08:59 +0300

    register flask restful and required routes

--- api/__init__.py
+++ api/__init__.py
@@ -1,23 +1,25 @@
 import os
+
 from flask import Flask, Blueprint
 from flask_jwt_extended import JWTManager
 from flask_restful import Api, Resource
 
-from api.v1.auth.sign_up import sign_up
-from api.v1.auth.log_in import log_in
-from api.v1.auth.reset import reset
-from api.v1.meetups.view_meetups import upcoming_meetups, specific_meetup, admin_meetup
-from api.v1.meetups.create_meetup import create_m
-from api.v1.meetups.rsvp import rsvp_m
-from api.v1.questions.post_questions import post_q
-from api.v1.questions.answer_question import answer_q
-from api.v1.questions.vote import upvote_q, downvote_q
+from .error_handlers import errors
+
+from .v1.auth.sign_up import Signup
+from .v1.auth.log_in import Login
+from .v1.auth.reset import Reset
+from .v1.meetups.meetups import Meetups, GetSpecificMeetup, GetAdminMeetups
+from .v1.meetups.rsvp import Rsvp
+from .v1.questions.post_questions import PostQuestion
+from .v1.questions.answer_question import AnswerQuestion
+from .v1.questions.vote import Upvote, DownVote
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     api_bp = Blueprint('api', __name__)
-    api = Api(api_bp)
+    api = Api(api_bp, errors=errors)
     jwt = JWTManager(app)
 
     # register blueprint
@@ -25,6 +27,8 @@ def create_app(test_config=None):
 
     app.config.from_mapping(
         SECRET_KEY='questioner-dev-secret-key',
+        JWT_SECRET_KEY='questioner-jwt-secret-key',
+        PROPAGATE_EXCEPTIONS=True
     )
 
     if test_config is None:
@@ -41,10 +45,22 @@ def create_app(test_config=None):
         pass
 
     class HelloWorld(Resource):
-        def get(self):
+        @staticmethod
+        def get():
             return {'hello': 'world'}
 
     # register routes
     api.add_resource(HelloWorld, '/')
+    api.add_resource(Signup, '/auth/signup')
+    api.add_resource(Login, '/auth/login')
+    api.add_resource(Reset, '/auth/reset')
+    api.add_resource(Meetups, '/meetups')
+    api.add_resource(GetSpecificMeetup, '/meetups/<int:m_id>')
+    api.add_resource(GetAdminMeetups, '/admin/profile/<int:uid>')
+    api.add_resource(Rsvp, '/meetups/<int:m_id>/rsvps')
+    api.add_resource(PostQuestion, '/questions')
+    api.add_resource(AnswerQuestion, '/questions/<int:q_id>/answer')
+    api.add_resource(Upvote, '/questions/<int:q_id>/upvote')
+    api.add_resource(DownVote, '/questions/<int:q_id>/downvote')
 
     return app
