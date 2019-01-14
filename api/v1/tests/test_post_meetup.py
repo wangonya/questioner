26a4a5c43b7c768e3499b5672c3a969554e45d9d
wangonya <kwangonya@gmail.com>
2019-01-14 06:05:02 +0300

    fix typo in test data and improve response assertions

--- api/v1/tests/test_post_meetup.py
+++ api/v1/tests/test_post_meetup.py
@@ -5,9 +5,10 @@ def test_post_meetup(main):
     test_data = {
             "title": "sample meetup",
             "location": "test location",
-            "happeningOn": "test date",
+            "happening_on": "test date",
             "tags": ["tag1", "tag2", "tag3"],
             "image": ""
         }
     res = post_json(main, '/api/v1/meetups', test_data)
     assert res.status_code == 201 or res.status_code == 401
+    assert b"meetup created successfully" in res.data or b"Only admins can access this route" in res.data
