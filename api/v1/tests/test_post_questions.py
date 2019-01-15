964a26ac5ed223e1d1db705386636e4cb8546f04
wangonya <kwangonya@gmail.com>
2019-01-15 23:01:03 +0300

    test that post question data passed into model is correct

--- api/v1/tests/test_post_questions.py
+++ api/v1/tests/test_post_questions.py
@@ -1,11 +1,21 @@
-from api.v1.tests import main, post_json
+import datetime
+
+from api.v1.tests import main, post_json, new_question
 
 
-def test_post_question(main):
+def test_post_question(main, new_question):
     test_data = {
         "title": "sample question",
         "body": "sample question body",
         "meetup": 1
     }
     res = post_json(main, '/api/v1/questions', test_data)
+    assert isinstance(new_question.q_id, int)
+    assert new_question.title == "sample question"
+    assert isinstance(new_question.creator, int)
+    assert new_question.body == "sample question body"
+    assert isinstance(new_question.meetup, int)
+    assert isinstance(new_question.created_on, datetime.datetime)
+    assert isinstance(new_question.meetup, int)
     assert res.status_code == 201
+    assert b"question submitted successfully" in res.data
