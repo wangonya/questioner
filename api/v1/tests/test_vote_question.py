6fcb9fa18f8d7040163849354ddb6df17e1e234c
wangonya <kwangonya@gmail.com>
2019-01-15 23:01:54 +0300

    test that post vote data passed into model is correct

--- api/v1/tests/test_vote_question.py
+++ api/v1/tests/test_vote_question.py
@@ -1,15 +1,17 @@
-from api.v1.tests import main, patch_json
+from api.v1.tests import main, patch_json, new_upvote, new_downvote
 
 
-def test_upvote_question(main):
+def test_upvote_question(main, new_upvote):
     test_data = {}
     res = patch_json(main, '/api/v1/questions/1/upvote', test_data)
     assert res.status_code == 201
+    assert isinstance(new_upvote, int)
     assert b"vote added successfully" in res.data
 
 
-def test_downvote_question(main):
+def test_downvote_question(main, new_downvote):
     test_data = {}
     res = patch_json(main, '/api/v1/questions/1/downvote', test_data)
     assert res.status_code == 201
+    assert isinstance(new_downvote, int)
     assert b"vote added successfully" in res.data
