79406e6184ab2ff9d2578a5d12617775ec3df56b
wangonya <kwangonya@gmail.com>
2019-01-16 07:52:01 +0300

    add duplicate data error handler

--- api/v1/utils/error_handlers.py
+++ api/v1/utils/error_handlers.py
@@ -2,26 +2,37 @@ from werkzeug.exceptions import HTTPException
 
 
 class UserAlreadyExistsError(HTTPException):
+    """handle duplicate user registration"""
     pass
 
 
 class InvalidEmailFormatError(HTTPException):
+    """handle invalid format"""
     pass
 
 
 class UserLoginError(HTTPException):
+    """hanle invalid login details"""
     pass
 
 
 class DataIndexError(HTTPException):
+    """handle data not found errors"""
     pass
 
 
 class AdminProtectedError(HTTPException):
+    """handle unauthorised access"""
     pass
 
 
 class InvalidPasswordLengthError(HTTPException):
+    """handle password length during signup"""
+    pass
+
+
+class DuplicateDataError(HTTPException):
+    """handle duplcate data entry"""
     pass
 
 
@@ -49,5 +60,9 @@ errors = {
     "AdminProtectedError": {
         "message": "Only admins can access this route",
         "status": 401,
+    },
+    "DuplicateDataError": {
+        "message": "The data entered already exists in the resource",
+        "status": 409,
     },
 }
