1f18166ea3f27a0d0aec30e5be1daa9e3f9f0479
wangonya <kwangonya@gmail.com>
2019-01-16 10:02:39 +0300

    handle post answer duplicates

--- api/v1/questions/answer_question.py
+++ api/v1/questions/answer_question.py
@@ -3,9 +3,11 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from ..questions.models import PostQuestionsModel, AnswerQuestionsModel
 from ..auth.models import AuthModel
+from ..utils.validators import AnswerValidators
 
 
 class AnswerQuestion(Resource):
+    """post answer endpoint resource"""
     parser = reqparse.RequestParser()
     parser.add_argument("body",
                         type=str,
@@ -14,6 +16,7 @@ class AnswerQuestion(Resource):
 
     @jwt_required
     def post(self, q_id):
+        """do a POST on the answers endpoint"""
         data = AnswerQuestion.parser.parse_args()
         body = data["body"]
 
@@ -22,6 +25,9 @@ class AnswerQuestion(Resource):
         creator = AuthModel.find_by_uid(user)
 
         meetup = PostQuestionsModel.find_meetup_by_q_id(q_id)
+
+        # handle duplicates
+        AnswerValidators.check_duplicate_answer(body, q_id)
 
         answer = AnswerQuestionsModel(body, creator, meetup, q_id)
         answer.save_answer_to_db()
