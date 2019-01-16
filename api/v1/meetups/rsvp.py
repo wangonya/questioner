aacc20062b0eb9256b51b703dcf80e72915b46eb
wangonya <kwangonya@gmail.com>
2019-01-16 10:36:18 +0300

    handle invalid status check

--- api/v1/meetups/rsvp.py
+++ api/v1/meetups/rsvp.py
@@ -2,9 +2,11 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from ..meetups.models import MeetupModel, RsvpsModel
 from ..auth.models import AuthModel
+from ..utils.validators import RsvpValidators
 
 
 class Rsvp(Resource):
+    """resource for Rsvp endpoint"""
     parser = reqparse.RequestParser()
     parser.add_argument("status",
                         type=str,
@@ -14,11 +16,15 @@ class Rsvp(Resource):
     # TODO: CHECK FOR AND HANDLE DUPLICATES
     @jwt_required
     def post(self, m_id):
+        """do POST on rsvp endpoint"""
         data = Rsvp.parser.parse_args()
         status = data["status"]
         user = get_jwt_identity()
         userid = AuthModel.find_by_uid(user)
         title = MeetupModel.find_title_by_m_id(m_id)
+
+        # validate status
+        RsvpValidators.check_proper_rsvp(status)
 
         rsvp = RsvpsModel(status, userid, m_id)
         rsvp.save_rsvp_to_db()
